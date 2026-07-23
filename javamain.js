0cec240535bddb9571f6c4d86fce39ebe865381e
ROC-1 <burgerriley123@gmail.com>
2026-07-23 17:41:14 +1000

    Minor upd

--- javamain.js
+++ javamain.js
@@ -5,6 +5,9 @@ sessionStorage.setItem("Testmode",0) // change in GITHUB
 window.c = console.log
 window.getel = function getel(el) {return document.getElementById(el)}
 
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getDatabase, ref, set, get, child } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
+
 window.gss = function gss(type) {
     if (type==1){return(Number(sessionStorage.getItem("Testmode")))}
     if (type==2){return(Number(sessionStorage.getItem("ISADMIN")))}
@@ -21,8 +24,20 @@ let TopBar1
 let TopBar2
 let TopBar3
 
-////CONTENT LOADED////
 document.addEventListener("DOMContentLoaded", () => {
+    const segments = document.querySelectorAll(".segment");
+
+    segments.forEach((segment, index) => {
+        segment.addEventListener("click", () => {
+            document.querySelector(".segment.active")
+                .classList.remove("active");
+            segment.classList.add("active");
+            if (index==0) {window.location.href = "index.html"}
+            if (index==1) {window.location.href = "Notes.html"}
+            if (index==2) {window.location.href = "Games.html"}
+        });
+    });
+
     if (gss(2) === 0) {
         if (gss(1)==1){sss(2, 1)} else {sss(2, 0)}
         if (gss(1)==1){sss(3, "Admin")} else {sss(3, 0)}
@@ -33,7 +48,6 @@ document.addEventListener("DOMContentLoaded", () => {
     }}
 
     const headline = document.getElementById("headline-1");
-    //TopDiv = document.getElementById("topDiv");
     TopBar = document.createElement("div");
     TopBar.className = "TopBar"
     document.body.appendChild(TopBar);
@@ -41,43 +55,6 @@ document.addEventListener("DOMContentLoaded", () => {
         if (gss(1)==1){sss(2, 1)} else {sss(2, 0)}
         if (gss(1)==1){sss(3, "Admin")} else {sss(3, 0)}
     }
-
-    headline.addEventListener("mouseenter", () => {
-        TopBar1 = document.createElement("div");
-        TopBar2 = document.createElement("div");
-        TopBar3 = document.createElement("div");
-        TopBar1.style.marginLeft="1px";TopBar2.style.marginLeft="1px";TopBar3.style.marginLeft="1px";
-        
-        TopBar1.innerHTML = '<b><a class="TopA" href="index.html">&nbsp;&nbsp;Term Calendar</a></b>';
-        TopBar1.className = "TopBarEl";
-        TopBar1.id = "TopBar1";
-        TopBar2.innerHTML = '<b><a class="TopA" href="Games.html">&nbsp &nbsp &nbsp &nbsp &nbspGames &nbsp &nbsp &nbsp &nbsp &nbsp</a></b>';
-        TopBar2.className = "TopBarEl";
-        TopBar2.id = "TopBar2";
-        TopBar3.innerHTML = '<b><a class="TopA" href="Notes.html">&nbsp &nbsp &nbsp &nbsp &nbsp Notes &nbsp &nbsp &nbsp &nbsp &nbsp</a></b>';
-        TopBar3.className = "TopBarEl";
-        TopBar3.id = "TopBar3";
-        /*
-        if (window.location.pathname.split("/").pop() == "index.html")
-            {TopBar1.id="TopBar1";TopBar2.id="TopBar2";TopBar3.id="TopBar3";}
-        else if (window.location.pathname.split("/").pop() == "Games.html")
-            {TopBar1.id="TopBar2";TopBar2.id="TopBar1";TopBar3.id="TopBar3";}
-        else {TopBar1.id="TopBar2";TopBar2.id="TopBar3";TopBar3.id="TopBar1";}*/
-
-        if (window.location.pathname.split("/").pop() == "Games.html")
-            {TopBar1.id="TopBar2";TopBar2.id="TopBar1";TopBar3.id="TopBar3";}
-        else if (window.location.pathname.split("/").pop() == "Notes.html")
-            {TopBar1.id="TopBar2";TopBar2.id="TopBar3";TopBar3.id="TopBar1";}
-        else {TopBar1.id="TopBar1";TopBar2.id="TopBar2";TopBar3.id="TopBar3";}
-        TopBar.appendChild(TopBar1);
-        TopBar.appendChild(TopBar2);
-        TopBar.appendChild(TopBar3);
-        TopBar.addEventListener("mouseleave", () => {
-            TopBar1.remove();
-            TopBar2.remove();
-            TopBar3.remove();
-        })
-    })
     setTimeout(()=> {
         if (gss(1)===null) {
             sss(1, 0)
@@ -87,4 +64,87 @@ document.addEventListener("DOMContentLoaded", () => {
             sss(3, localStorage.getItem("UserLocal") || 0)
         }
     }, 2000)
+    Loop()
 })
+
+window.NotLoggedIn = NotLoggedIn;
+function NotLoggedIn() {
+    if (gss(3)==0) {
+        getel("LogInBg").hidden = false
+    } else {
+        getel("LogOut").hidden = !getel("LogOut").hidden
+    }
+}
+window.LogIn = LogIn;
+function LogIn() {
+    let uservalue = getel("UserInput").value
+    if (uservalue != "Admin" && uservalue != "" && getel("PasswordInput").value != "") {
+        const dbRef = ref(db);
+        get(child(dbRef, `${uservalue}/Profile/Password`)).then((pass) => {
+            if (pass.val() == getel("PasswordInput").value || !pass.exists()) {
+                password = getel("PasswordInput").value;
+                getel("LogInBg").hidden = true
+                sss(3, getel("UserInput").value)
+                getel("LoggedIn").innerText = "Logged: "+gss(3);
+                localStorage.setItem("UserLocal", gss(3))
+                if (loadclicked) {
+                    StartLoad()
+                }
+            } else {
+                getel("PasswordInput").value = ""
+                getel("PasswordInput").placeholder = "Incorrect Password"
+            } 
+        })
+    }
+}
+window.CancelLogIn = CancelLogIn;
+function CancelLogIn() {
+    getel("LogInBg").hidden = true
+}
+window.LogOut = LogOut;
+function LogOut() {
+    localStorage.setItem("UserLocal", 0); sss(3, 0); sss(2, 0);
+    TDList = {}; loadTDL();
+    dayStates = {}; buildCalendar()
+    getel("LogOut").hidden = true
+}
+
+function Loop() {
+    if (getel("LogInBg").hidden == false) {
+        if (getel("UserInput").value != "" && getel("PasswordInput").value != "") {
+            getel("LogIn").setAttribute("Ready","1")}
+        else {getel("LogIn").setAttribute("Ready","0")}
+    }
+    if (gss(3)!="" && gss(3) != 0) {
+        getel("LoggedIn").innerHTML = "Logged In: <b>"+gss(3)
+        if (gss(3) == "Admin") {
+            getel("LoggedIn").innerHTML = "Welcome back Riley"
+        }
+    } else {
+        getel("LoggedIn").innerHTML = "Not Loggen In"
+    }
+    if (gss(1)==1) {getel("LoggedIn").innerText = "In Test Mode"}
+    setTimeout(Loop, 400)
+}
+
+document.addEventListener("click", (e) => {
+    mousex = e.pageX;
+    mousey = e.pageY;
+    if (e.target === document.body || e.target === document.documentElement) {
+        getel("LogOut").hidden = true
+    }
+})
+
+
+window.firebaseConfig = {
+    apiKey: "AIzaSyBVOC6RVvQw2V7YSN8MF24kM0p9N1tcfTo",
+    authDomain: "calendar-5487e.firebaseapp.com",
+    databaseURL: "https://calendar-5487e-default-rtdb.firebaseio.com",
+    projectId: "calendar-5487e",
+    storageBucket: "calendar-5487e.firebasestorage.app",
+    messagingSenderId: "705086479682",
+    appId: "1:705086479682:web:5511ad5b3e00921947bcdb",
+    measurementId: "G-CGMRRC6B0B"
+};
+window.app = initializeApp(firebaseConfig);
+window.db = getDatabase(app);
